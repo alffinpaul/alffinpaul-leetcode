a5e2596341b00a474f7139370e1d484955d5ee9a
alffinpaul <alffin1234@gmail.com>
2026-05-11 12:07:50 +0530

    Time: 2 ms (84.21%), Space: 150.8 MB (31.58%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.dart
@@ -0,0 +1,16 @@
+class Solution {
+ List<int> countBits(int n) {
+  List<int> ans = List.filled(n + 1, 0);
+
+  for (int i = 1; i <= n; i++) {
+    ans[i] = ans[i >> 1] + (i & 1);
+  }
+
+  return ans;
+}
+
+void main() {
+  int n = 2;
+  print(countBits(n)); // [0, 1, 1]
+}
+}
