20b0fa883515223311c4512665b7a6a768c8911b
alffinpaul <alffin1234@gmail.com>
2026-06-19 14:24:13 +0530

    Time: 2 ms (50%), Space: 151.8 MB (20%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.dart
@@ -0,0 +1,18 @@
+class Solution {
+  List<int> diStringMatch(String s) {
+    int low =0;
+    int high = s.length;
+    List<int> ans=[];
+    for(int i =0;i<s.length;i++){
+        if(s[i]=='I'){
+            ans.add(low);
+            low++;
+        }else{
+            ans.add(high);
+            high--;
+        }
+    }
+    ans.add(low);
+    return ans;
+  }
+}
