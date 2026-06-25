8a14e2743fa694ebb679c01e5de62082656c67fa
alffinpaul <alffin1234@gmail.com>
2026-06-25 23:56:55 +0530

    Time: 1 ms (100%), Space: 149 MB (100%) - LeetHub

---
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.dart
@@ -0,0 +1,17 @@
+class Solution {
+  int vowelStrings(List<String> words, int left, int right) {
+    int count = 0;
+    String vowels = "aeiou";
+
+    for (int i = left; i <= right; i++) {
+      String word = words[i];
+
+      if (vowels.contains(word[0]) &&
+          vowels.contains(word[word.length - 1])) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+}
